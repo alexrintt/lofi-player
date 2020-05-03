7ceee1d1fd4e69789a8f19c27ceed23f93d7885c
LaksCastro <lakhsmycastro336@gmail.com>
2020-05-03 17:42:03 -0300

    fix gitignore and add pre build

--- publish.js
+++ publish.js
@@ -1,6 +1,6 @@
 const gh = require("gh-pages");
 
-gh.publish("", (err) => {
+gh.publish("/dist", (err) => {
   if (err) throw err;
 
   console.log("\x1b[36m%s\x1b[0m", "Sucess Publish! Congratulations!");
