fd94a6dcf969772d5aee4c02606d962c4b2f430d
lask <lakhsmycastro336@gmail.com>
2022-02-13 20:58:58 -0300

    Fix typo

--- publish.js
+++ publish.js
@@ -3,5 +3,5 @@ const gh = require("gh-pages");
 gh.publish("dist", (err) => {
   if (err) throw err;
 
-  console.log("\x1b[36m%s\x1b[0m", "Sucess Publish! Congratulations!");
+  console.log("\x1b[36m%s\x1b[0m", "Success Publish! Congratulations!");
 });
